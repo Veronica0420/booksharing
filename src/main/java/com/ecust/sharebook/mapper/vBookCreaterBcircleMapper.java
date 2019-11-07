226454419cd452a78801ad2261302ff1dcd363c1
Sijar <1278824488@qq.com>
2019-11-07 14:36:37 +0800

    提交二

---
+++ src/main/java/com/ecust/sharebook/mapper/vBookCreaterBcircleMapper.java
@@ -0,0 +1,18 @@
+package com.ecust.sharebook.mapper;
+
+import com.ecust.sharebook.pojo.vBookCreaterBcircle;
+
+import java.util.List;
+import java.util.Map;
+
+public interface vBookCreaterBcircleMapper {
+
+    int deleteByPrimaryKey(Map<String, Object> map);
+
+    List<vBookCreaterBcircle> getBaseResultKey(Map<String, Object> map);
+
+    vBookCreaterBcircle selectByPrimaryKey(Map<String, Object> map);
+
+    //根据书名查找有此书的图书圈  ——Sijar
+    List<Integer> selectByBookName(Map<String, Object> map);
+}
